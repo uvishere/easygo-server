83646062ef7a560d9121d3b1e39bbf769d87dbe7
Yuba Raj Panta <uvishere@gmail.com>
2019-04-14 01:49:04 +0930

    database user/pass changed to options

--- src/db/dbconfig.js
+++ src/db/dbconfig.js
@@ -5,5 +5,7 @@ const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useFindAndModify: false
+    useFindAndModify: false,
+    user: process.env.DATABASE_USER,
+    pass: process.env.DATABASE_PASSWD,
 })
