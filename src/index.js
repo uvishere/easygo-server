2c6b2fd83b4f9bf99d91228e4eab6c42026a57f8
Yuba Raj Panta <uvishere@gmail.com>
2019-04-14 01:59:45 +0930

    dotenv config path added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 "use strict";
 
-require('dotenv').config()
+require('dotenv').config({ path: '../.env' })
 const express = require('express')
 require('./db/dbconfig')
 const userRouter = require('./routers/user')
