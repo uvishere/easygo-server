9353b86ff9741d443cd71d188ec1d26ef27b26a1
Yuba Raj Panta <uvishere@gmail.com>
2019-04-14 03:12:21 +0930

    tried setting up travis CD flow

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env sh
-set -x
 
-tar -czf package.tgz build && \
-scp package.tgz $REMOTE_USER@$REMOTE_HOST:$REMOTE_APP_DIR && \
-ssh $REMOTE_USER@$REMOTE_HOST 'bash -s' < ./scripts/untar.sh
+ssh $REMOTEUSER@$REMOTEHOST
+cd  $REMOTEAPPDIR
+git checkout .
+git pull
+pm2 restart easygo
+
+# tar -czf package.tgz build && \
+# scp package.tgz $REMOTE_USER@$REMOTE_HOST:$REMOTE_APP_DIR && \
+# ssh $REMOTE_USER@$REMOTE_HOST 'bash -s' < ./scripts/untar.sh
